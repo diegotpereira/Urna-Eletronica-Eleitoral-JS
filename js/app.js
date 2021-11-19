779322d89c03da990df9f9801b4ae32bbc51932c
diego <diegoteixeirapereira@hotmail.com>
2021-11-19 17:39:32 -0300

    criação dos arquivos javascript.

---
+++ js/app.js
@@ -0,0 +1,42 @@
+let seuVotoPara = document.querySelector('.d-1-1 span')
+let cargo = document.querySelector('.d-1-2')
+let descricao = document.querySelector('.d-1-4')
+let aviso = document.querySelector('.d-2')
+let lateral = document.querySelector('.d-1-right')
+let numeros = document.querySelector('.d-1-3')
+
+let etapaAtual = 0
+
+function comecarEtapa() {
+
+    let etapa = etapas[etapaAtual]
+
+    let numeroHtml = ''
+
+    seuVotoPara.style.display = 'none'
+
+    cargo.innerHTML = etapa.titulo
+    descricao.innerHTML = ''
+    aviso.style.display = 'none'
+    lateral.innerHTML = ''
+    numeros.innerHTML = numeroHtml
+
+}
+
+function clicou(n) {
+    alert("Clicou em " + n)
+}
+
+function branco() {
+    alert("Clicou em branco!")
+}
+
+function corrige() {
+    alert("Clicou em corrige!")
+}
+
+function confirma() {
+    alert("Clicou em confirma!")
+}
+
+comecarEtapa()
---
+++ js/candidatos.js
@@ -0,0 +1,47 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '38111',
+                nome: 'Leandro Facim',
+                partido: 'PROGRAMADORES',
+                fotos: [
+                    { url: '381111.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Robert Lucas',
+                partido: 'SOFTWARES',
+                fotos: [
+                    { url: '77222.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '15',
+                nome: 'William Souza',
+                partido: 'DESENVOLVEDORES',
+                vice: 'Kelly Amaral',
+                fotos: [
+                    { url: '15.jpg', legenda: 'Prefeito' },
+                    { url: '15_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '54',
+                nome: 'Luiz Fernando',
+                partido: 'REDES',
+                vice: 'Rominho',
+                fotos: [
+                    { url: '54.jpg', legenda: 'Prefeito' },
+                    { url: '54_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
